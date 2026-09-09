d5e2d7a78d9a205268f253e2b56d23d3f708212f
samuel felipe <samuel.f.cardoso6@gmail.com>
2026-09-09 08:06:05 -0300

    escolha feita

--- main.py
+++ main.py
@@ -5,3 +5,14 @@ print("---- Cardapio ----")
 
 for indice in range(len(pizzas)):
     print(f"{indice +1} - {pizzas[indice]}: R${precos[indice]}")
+escolha= input("digite sua escolha: ")
+if escolha.isdigit():
+    escolha=int(escolha)
+    if 1 <= escolha <= len(pizzas):
+        indice = escolha - 1
+        print(f"você escolheu: {pizzas[indice]}")
+        print(f"Preço: R$ {precos[indice]:.2f}")
+    else:
+        print("Opção inválida! Escolha um número do cardápio.")
+else:
+    print("Digite apenas o número da pizza.")
